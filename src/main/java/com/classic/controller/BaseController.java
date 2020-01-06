ede6a6ed18346eef1710d51e3ab1b7a73c1f5ddd
NoApologize <liujl2573@163.com>
2020-01-06 10:38:45 +0800

    --

--- src/main/java/com/classic/controller/BaseController.java
+++ src/main/java/com/classic/controller/BaseController.java
@@ -2,30 +2,25 @@ package com.classic.controller;
 
 import com.classic.service.BaseService;
 import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/base")
 public class BaseController {
 
     @Autowired
     private BaseService baseService;
 
-    @ResponseBody
     @RequestMapping("/begin")
     public String begin() {
-        System.out.printf("0000");
         return baseService.begin();
     }
 
-    @ResponseBody
-    @RequestMapping("/getList")
+    @GetMapping("/getList")
     public String getList() {
-
         return JSONArray.fromObject(baseService.getList()).toString();
     }
 
